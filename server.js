0442d40d1da6d73ae0086b1c0363f5442f153c11
Gabriel Montes <gabriel@bloq.com>
2019-07-18 22:22:24 -0300

    Prevent bot to start

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 'use strict'
 
-const startMonitor = require('./src')
+// const startMonitor = require('./src')
+
+function startMonitor () {
+  setTimeout(() => undefined, 24 * 60 * 60 * 1000)
+}
 
 startMonitor()
