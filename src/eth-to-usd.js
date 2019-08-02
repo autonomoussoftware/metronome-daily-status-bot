f66688276413283218c002bcdc489036c15fe957
Unknown <patidarmanoj@gmail.com>
2019-08-02 23:25:23 +0530

    updated coincap api

--- src/eth-to-usd.js
+++ src/eth-to-usd.js
@@ -1,10 +1,8 @@
 'use strict'
 
 const pRetry = require('p-retry')
-const coincap = require('coincap-lib')
 const config = require('config')
-
+const fetch = require('isomorphic-fetch')
 const retry = fn => pRetry(fn, { retries: config.coincap.retries })
-const toUSD = eth => retry(() => coincap.coin(config.eth.symbol).then(res => res.price * eth))
-
+const toUSD = eth => retry(() => fetch(`${config.coincap.baseUrl}/rates/${config.eth.id}`).then(response => response.json().then(res => res.data.rateUsd * eth)))
 module.exports = toUSD
---
+++ test/test-eth-rates.js
@@ -0,0 +1,3 @@
+'use strict'
+const toUSD = require('../src/eth-to-usd.js')
+toUSD(2).then(console.log)
