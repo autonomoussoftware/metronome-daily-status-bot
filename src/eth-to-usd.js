0c7717624755dfd89d835b9b761fc41fe0a6ba05
Manoj <patidarmanoj@gmail.com>
2019-08-09 20:38:06 +0530

    fixed issue of retry

--- src/eth-to-usd.js
+++ src/eth-to-usd.js
@@ -4,6 +4,6 @@ const pRetry = require('p-retry')
 const config = require('config')
 const { BittrexClient } = require('bittrex-node')
 const client = new BittrexClient()
-const retry = fn => pRetry(fn, { retries: config.coincap.retries })
+const retry = fn => pRetry(fn, { retries: config.retries })
 const toUSD = eth => retry(() => client.ticker(`USD-${config.eth.symbol}`).then(res => res.Last * eth))
 module.exports = toUSD
